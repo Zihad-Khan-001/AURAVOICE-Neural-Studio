c9d8b6b205a6118aedc2eabaf133ec7fedbeebb1
Zihad-Khan-001 <zihhhhd5@gmail.com>
2026-08-22 08:11:29 +0600

    Initialize AURAVOICE project structure

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AURAVOICE-Neural-Studio"
+
+include(":app")
+include(":audio-engine")
